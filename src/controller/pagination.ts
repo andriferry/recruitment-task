e2c243307e9399c844b2761355bbb2d4f295dc4c
andriferry <ferryandri35@gmail.com>
2021-09-12 09:17:37 +0700

    Add function to pagination

--- src/controller/pagination.ts
+++ src/controller/pagination.ts
@@ -1,17 +1,33 @@
 export default function usePagination(dataTask: any) {
 
 
-    const pagination = (start: number, end?: number) => {
 
-    if (typeof dataTask.sortData == "undefined") {
-      dataTask.task  = dataTask.allTasks.slice(start, end)
-    } else { 
-      dataTask.sortData  = dataTask.allTasks.slice(start, end)
-    }
+  const nextPage = ((page: string) => {
 
+  })
+
+
+  const previousPage = ((page: string) => {
+
+  })
+
+
+
+  const toPagination = () => {
     
   }
+
+
+  const pagination = (start: number, end?: number) => {
+      if (typeof dataTask.sortData == "undefined") {
+        dataTask.task  = dataTask.allTasks.slice(start, end)
+      } else { 
+        dataTask.sortData  = dataTask.allTasks.slice(start, end)
+      }
+  }
     
-    return {pagination}
+  
+  
+  return { pagination }
     
 }
