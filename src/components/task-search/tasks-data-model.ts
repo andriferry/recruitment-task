03c77b2457d29e8686b91d6ac0ca256a9a5f6099
andriferry <ferryandri35@gmail.com>
2021-09-20 14:18:09 +0700

    remove duplicate platform

--- src/components/task-search/tasks-data-model.ts
+++ src/components/task-search/tasks-data-model.ts
@@ -5,15 +5,25 @@ import fetch from '@/components/task-search/get-taks'
 export default function useTask() {
   const { getTasks } = fetch()
   const params = reactive<ParamsData>({
-    limit: 6,
+    limit: 100,
   })
   const allTasks = ref()
   const tasks = ref()
+  const allPlatforms = ref(['OTHER'])
+
+  const removeDuplicatePlatform = () => {
+    const platforms = allTasks.value
+      .map((element: any) => element.platforms)
+      .flat()
+    
+  }
 
   onMounted(() => {
     getTasks(params).then((res) => {
       allTasks.value = res.data.tasks
       tasks.value = res.data.tasks.slice(0, 3)
+      //removeDuplicatePlatform(res.data.tasks)
+      removeDuplicatePlatform()
     })
   })
 
