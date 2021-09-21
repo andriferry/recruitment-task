e5deddf8df1f4ba423cfbc3c47853b1e77e1e61a
andriferry <ferryandri35@gmail.com>
2021-09-21 13:24:00 +0700

    Add existing query function

--- src/controller/handle-routes.ts
+++ src/controller/handle-routes.ts
@@ -1,8 +1,13 @@
 import { useRouter } from 'vue-router'
+import { onMounted } from 'vue'
 
 export default function useHandleRoute() {
   const router = useRouter()
   const pathLocation = router.currentRoute.value.path
+
+  const existingQuery = () => {
+    console.log('Jell')
+  }
 
   const queryPlatform = (platform: string) => {
     router.push({ path: pathLocation, query: { platform } })
@@ -19,6 +24,19 @@ export default function useHandleRoute() {
   const queryKeyword = (keyword: string) => {
     router.push({ path: pathLocation, query: { keyword } })
   }
+
+  onMounted(() => {
+    console.log(router.currentRoute.value.query)
+
+    const get = router.currentRoute.value.query
+
+    for (const key in get) {
+      if (Object.prototype.hasOwnProperty.call(get, key)) {
+        const element = get[key]
+        console.log(element)
+      }
+    }
+  })
 
   return { queryPlatform, queryKeyword, queryGreaterBudget, queryLowerBudget }
 }
